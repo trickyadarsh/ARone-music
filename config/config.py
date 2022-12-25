e79b23441fd4192d8164ea26618f34ada73a80bf
trickyadarsh <121372273+trickyadarsh@users.noreply.github.com>
2022-12-25 05:27:49 -0800

    Update config.py

--- config/config.py
+++ config/config.py
@@ -11,7 +11,7 @@ API_ID = int(getenv("API_ID", "19893481"))
 API_HASH = getenv("API_HASH", "695b2e447b8aeb79966ff572210b0bc1")
 BOT_TOKEN = getenv("BOT_TOKEN", "5625397650:AAH90ledMbxHZSiXUJtqC9U2vJpv2TKr68U")
 MONGO_DB_URI = getenv("MONGO_DB_URI", "mongodb+srv://rajputt123:956981@cluster0.ssjtqq1.mongodb.net/?retryWrites=true&w=majority")
-LOG_GROUP_ID = int(getenv("LOG_GROUP_ID", "-1001810356172"))
+LOG_GROUP_ID = int(getenv("LOG_GROUP_ID", "-1001551699668"))
 MUSIC_BOT_NAME = getenv("MUSIC_BOT_NAME", "𝐑𝐀𝐨𝐧𝐞𝐗")
 OWNER_ID = list(map(int, getenv("OWNER_ID", "2033800689").split()))
 SUPPORT_CHANNEL = getenv(
