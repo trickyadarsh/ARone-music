6de5bf781ee06c9c6a24030d40b7dfe44cabb893
trickyadarsh <121372273+trickyadarsh@users.noreply.github.com>
2022-12-25 01:12:45 -0800

    Update start.py

--- LoverMusic/utils/inline/start.py
+++ LoverMusic/utils/inline/start.py
@@ -4,17 +4,19 @@ from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 
 import config
 from LoverMusic import app
+
+
 def start_pannel(_, BOT_USERNAME, OWNER: Union[bool, int] = None):
     buttons = [
         [
             InlineKeyboardButton(
-                text="⁣𓆩𝗔𝗱𝗱 𝗠𝗲 𝗧𝗼 𝗬𝗼𝘂𝗿 𝗴𝗿𝗼𝘂𝗽𓆪",
+                text="⁣𓆩𝗔𝗗𝗗 𝗠𝗘𓆪",
                 url=f"https://t.me/{BOT_USERNAME}?startgroup=true",
             )
         ],
         [
             InlineKeyboardButton(
-                text="𝗖𝗼𝗺𝗺𝗮𝗻𝗱𝘀",
+                text="𝗰𝗼𝗺𝗺𝗮𝗻𝗱𝘀 𝗰𝗵𝗲𝗮𝗸",
                 callback_data="settings_back_helper",
             ),
             InlineKeyboardButton(
@@ -23,9 +25,9 @@ def start_pannel(_, BOT_USERNAME, OWNER: Union[bool, int] = None):
         ],
         [
             InlineKeyboardButton(
-                text="𝗔𝗟𝗟 𝘂𝗽𝗱𝗮𝘁𝗲", url=f"https://t.me/Lover_Music_Support"),
+                text="𝗔𝗹𝗹 𝘂𝗽𝗱𝗮𝘁𝗲𝘀", url=f"https://t.me/Lover_Music_Support"),
             InlineKeyboardButton(
-                text="𝗺𝗮𝗶𝗻 𝘀𝘂𝗽𝗽𝗼𝗿𝘁 𝗚", url=f"{config.SUPPORT_GROUP}"
+                text="𝗠𝗮𝗶𝗻 𝘀𝘂𝗽𝗽𝗼𝗿𝘁 𝗚", url=f"{config.SUPPORT_GROUP}"
             ),
         ],
      ]
@@ -36,23 +38,25 @@ def private_panel(_, BOT_USERNAME, OWNER: Union[bool, int] = "https://t.me/shubh
     buttons = [
         [
             InlineKeyboardButton(
-                text="𓆩𝗔𝗱𝗱 𝗠𝗲 𝗧𝗼 𝗬𝗼𝘂𝗿 𝗴𝗿𝗼𝘂𝗽𓆪",
+                text="𓆩𝗔𝗗𝗗 𝗠𝗘𓆪",
                 url=f"https://t.me/{BOT_USERNAME}?startgroup=true",
             ),
         ],
         [
             InlineKeyboardButton(
-                text="𝗖𝗼𝗺𝗺𝗮𝗻𝗱𝘀", callback_data="settings_back_helper"),
-            InlineKeyboardButton(
-                    text="𓊈𝗥𝗔𝗼𝗻𝗲𓊉", url=f"https://t.me/shubhamsah1"}"
-                
-        ],
-        [
-            InlineKeyboardButton(text="𝗔𝗟𝗟 𝘂𝗽𝗱𝗮𝘁𝗲", url=f"https://t.me/Lover_Music_Support"),
-            InlineKeyboardButton(
-                text="𝗺𝗮𝗶𝗻 𝘀𝘂𝗽𝗽𝗼𝗿𝘁 𝗚", url=f"{config.SUPPORT_GROUP}"
+                text="𝗰𝗼𝗺𝗺𝗮𝗻𝗱𝘀 𝗰𝗵𝗲𝗮𝗸", callback_data="settings_back_helper"
             ),
         ],
-    
+        [
+            InlineKeyboardButton(text="𝗔𝗹𝗹 𝘂𝗽𝗱𝗮𝘁𝗲𝘀", url=f"https://t.me/Lover_Music_Support"),
+            InlineKeyboardButton(
+                text="𝗠𝗮𝗶𝗻 𝘀𝘂𝗽𝗽𝗼𝗿𝘁 𝗚", url=f"{config.SUPPORT_GROUP}"
+            ),
+        ],
+        [
+            InlineKeyboardButton(
+                    text="𝗥𝗔𝗼𝗻𝗲", url=f"https://t.me/shubhamsah1"
+                )
+        ],
      ]
     return buttons
